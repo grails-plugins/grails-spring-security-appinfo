7a8ea3a76ead0bb520cf83b8bb42973d8373affd
Burt Beckwith <burt@burtbeckwith.com>
2013-10-05 15:00:24 -0400

    changed package to grails.plugin.spring_security_appinfo to be consistent with the core plugin

--- grails-app/controllers/grails/plugin/spring_security_appinfo/SecurityInfoController.groovy
+++ grails-app/controllers/grails/plugin/spring_security_appinfo/SecurityInfoController.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package grails.plugins.spring_security_appinfo
+package grails.plugin.spring_security_appinfo
 
 import grails.plugin.springsecurity.SpringSecurityUtils
 
