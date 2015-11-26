aa76b433da57337ec79fb0d64b177f4b74c21820
Burt Beckwith <burt@burtbeckwith.com>
2015-11-26 00:02:11 -0500

    build updates

--- release.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build/release
-mkdir -p build/release
-cd build/release
-git clone git@github.com:grails-plugins/grails-spring-security-appinfo.git
-cd grails-spring-security-appinfo
-grails clean
-grails compile
-
-./gradlew bintrayUpload --stacktrace
